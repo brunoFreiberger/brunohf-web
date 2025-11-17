bdb2107e36ae27dd5d7a84a89a7d52bd446a33d2
bhfreiberger <brunohfreiberger@gmail.com>
2025-11-17 00:01:44 -0300

    chore: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,65 @@
-import Image from "next/image";
+import {Separator} from "@/components/ui/separator";
+import {ThemeToggle} from "@/components/theme-toggle";
+import {Button} from "@/components/ui/button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    return (
+        <div className="w-full mx-auto">
+            <div className="flex w-full p-4 justify-between">
+                <div className="w-1/4">brunohf.dev</div>
+                <div className="justify-center items-center flex gap-4">
+                    <Button>About me</Button>
+                    <Button>Portfolio</Button>
+                    <Button>Blog</Button>
+                </div>
+                <div className="flex gap-4 w-1/4 justify-end">
+                    <ThemeToggle />
+                    <Button>Contact-me</Button>
+                </div>
+            </div>
+            <div className="flex justify-center items-center h-screen">
+                <div className="w-1/3 bg-amber-500 h-full flex justify-end">
+                    <div className="p-4 items-center flex flex-col">
+
+                        <Avatar className="h-24 w-24">
+                            <AvatarImage src="https://media.licdn.com/dms/image/v2/D4D03AQH3J9KWmsMEhw/profile-displayphoto-scale_200_200/B4DZpmaZWVJUAY-/0/1762654788361?e=1764806400&v=beta&t=LKWEGhLmgKwiRSAZm2RGrlyFTWxyonjBsRdRV9XQy4k" alt="@shadcn" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+
+                        <h4>Bruno Henrique Freiberger</h4>
+                        <h5>(Dedicated Full-Stack Engineer and Problem Solver)</h5>
+
+                        <Separator className="bg-red-600"/>
+
+                        <h5>Social Media</h5>
+
+                        <Separator className="bg-red-600"/>
+
+                        <h5>Tech Stack</h5>
+                    </div>
+                </div>
+                <Separator orientation="vertical" className="bg-red-600"/>
+                <div className="w-2/3 bg-amber-950 h-full">
+                    <div className="p-4">
+                        <div className="flex justify-center w-full">
+                            <div className="flex flex-col justify-center items-center">
+                                <h1>Full-Stack Engineer</h1>
+                                <h3>(Java, Spring Boot, JavaScript, React, Next.js, Node, Golang, Angular)</h3>
+                                <br/>
+                                <span className="w-2/4 text-center">
+                                    Experienced Full-Stack Developer with strong expertise in Java (Spring Boot) and JavaScript (Angular, React, Node.js). Passionate about building scalable, reliable, and customer-centric solutions that deliver real business impact.
+                                </span>
+                                <br/>
+                                <div className="flex justify-center items-center gap-4">
+                                    <Button>See my recommendations</Button>
+                                    <Button>Download CV</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+);
 }
